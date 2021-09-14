35a8449f45eb1857b2d37ec201930151205455f6
Bartosz Paluch <BarPal@Pracownia.com>
2021-09-14 11:39:23 +0200

    dwuwymiarowe

---
+++ lekcja/lekcja/lekcja.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int tab[2][2];
+
+    for (int i = 0; i < 10; i++) {
+
+        for (int x = 0; x < 10; x++) {
+
+            if (i == x) {
+                tab[i][x] = x;
+            }
+            else {
+                tab[i][x] = 0;
+            }
+            cout << tab[i][x] << " ";
+        }
+        cout << endl;
+    }
+
+
+}
+
